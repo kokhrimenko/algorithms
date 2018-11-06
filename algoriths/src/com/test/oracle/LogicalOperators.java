44a46477791dab4f37f0ec2c28e065a742f719cf
Kostiantyn Okhrimenko <kostya@bind.ee>
2018-11-06 14:10:18 +0200

    added some classes from oracle test

---
+++ algoriths/src/com/test/oracle/LogicalOperators.java
@@ -0,0 +1,26 @@
+package com.test.oracle;
+
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+
+        int spiderMan = 10;
+        int venom = 5;
+        int carnage = 50;
+
+        if (spiderMan == 11 & venom++ == 5) {
+            venom++;
+        }
+
+        if (++carnage == 50 | spiderMan++ == 11 | spiderMan++ == 12 | 
+                              spiderMan++ == 13 | spiderMan++ == 14) {
+            venom++;
+        }
+
+        if (++spiderMan == 14 | spiderMan++ == 15 & venom++ == 6 & spiderMan == 13) {
+            spiderMan++;
+        }
+        System.out.println(spiderMan + venom + carnage);
+    }
+
+}
