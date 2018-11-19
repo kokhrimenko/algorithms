dffd587de3d643b910f3012d7b46f9561e76b93b
Kostiantyn Okhrimenko <kostya@bind.ee>
2018-11-19 09:18:06 +0200

    interesting example with Scanner

---
+++ algoriths/src/com/test/oracle/TokenizerChallenge.java
@@ -0,0 +1,19 @@
+package com.test.oracle;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class TokenizerChallenge {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner("ThisIsIt,theFinalString,NoBugsProject");
+       
+       Pattern pattern = Pattern.compile("[^\\w*]");
+       sc.useDelimiter(pattern);
+      
+       while (sc.hasNext()) {
+           System.out.println(sc.next());
+       }
+
+       sc.close();
+    }
+}
