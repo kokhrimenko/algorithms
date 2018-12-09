81d86d330a71d31d34c526d511444e6f589046b3
kokhrimenko <kostya@bind.ee>
2018-12-09 18:29:33 +0200

    interesting example of throwing null as a parameter.

---
+++ algoriths/src/com/test/oracle/DevGymException.java
@@ -0,0 +1,23 @@
+package com.test.oracle;
+public class DevGymException {
+
+    private static IllegalArgumentException illegalArgument;
+
+    public static void main(String... doYourBest) {
+        try {
+            double exceptionNumber = 2 / 0.0;
+            System.out.println("exceptionNumber");
+
+            throw illegalArgument;
+        } catch (NullPointerException nullPointer) {
+            System.out.println("NullPointer");
+        } catch (IllegalArgumentException illegalArgument) {
+            System.out.println("IllegalArgument");
+        } catch (Exception e) {
+            System.out.println("Exception");
+        } finally {
+            System.out.println("Finally");
+        }
+    }
+
+}
