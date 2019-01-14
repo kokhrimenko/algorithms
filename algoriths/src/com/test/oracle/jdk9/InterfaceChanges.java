6fe533ce4970ace39c57a8bcdd7f593a06d7afe4
kokhrimenko <kostya@bind.ee>
2019-01-14 14:42:21 +0200

    interesting example of interface with some overridings

---
+++ algoriths/src/com/test/oracle/jdk9/InterfaceChanges.java
@@ -0,0 +1,34 @@
+package com.test.oracle.jdk9;
+
+import java.util.function.Function;
+
+public class InterfaceChanges {
+
+	interface Jedi {
+		String MASTER = "Yoda";
+		
+		default String attack() {
+			return jump(jedi -> String.join(jedi, useSaber(), useForce()));
+		}
+		
+		private String jump(Function<String, String> function) {
+			return function.apply("Luke");
+		}
+		
+		private static String useSaber() {
+			return "S";
+		}
+		
+		private static String useForce() {
+			return "F";
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(new Jedi() {
+			public String useForce() {
+				return "X";
+			}			
+		}.attack() + Jedi.useSaber() + Jedi.MASTER);
+	}
+}
